500093e2770d0b742538ee09cef4d8245768c4ed
unknown <wuchouchouer@163.com>
2023-05-06 21:42:08 +0800

    update cut_score()

--- R/cut_score.R
+++ R/cut_score.R
@@ -4,6 +4,7 @@
 #' @param breaks A numeric vector indicating the cut points, left open and right closed.
 #' @param values A vector indicating the return values of each segment, its length must be equal to `breaks` + 1.
 #' @param right Logical, indicating if the intervals should be closed on the right (and open on the left) or vice versa.
+#' @param if_na A value of the same type as `values` used to determine the value returned when x is NA.
 #'
 #' @return A value from `values`.
 #' @export
@@ -12,14 +13,18 @@
 #' cut_value(2, 1, c(1, -1))
 #' cut_value(2, 1, LETTERS[1:2])
 #' cut_value(2, c(1, 3), c(1, 3, 5))
-cut_value <- function(x, breaks, values, right = TRUE) {
+cut_value <- function(x, breaks, values, right = TRUE, if_na = NA) {
 
   if (length(values) != length(breaks) + 1) {
     stop("The length of `values` must be equal to the length of `breaks` + 1.")
   }
 
-  breaks <- c(-Inf, breaks, Inf)
-  index <- cut(x, breaks = breaks, labels = FALSE, right = right)
+  if (is.na(x)) {
+    return(if_na)
+  } else {
+    breaks <- c(-Inf, breaks, Inf)
+    index <- cut(x, breaks = breaks, labels = FALSE, right = right)
 
-  values[[index]]
+    values[[index]]
+  }
 }
--- tests/testthat/test-cut_score.R
+++ tests/testthat/test-cut_score.R
@@ -2,5 +2,7 @@ test_that("multiplication works", {
   expect_equal(cut_value(2, 1, c(1, -1)), -1)
   expect_equal(cut_value(2, 1, LETTERS[1:2]), "B")
   expect_equal(cut_value(2, c(1, 3), c(1, 3, 5)), 3)
+  expect_equal(cut_value(NA, 1, LETTERS[1:2], if_na = "Z"), "Z")
+  expect_equal(cut_value(NA, c(1, 3), c(1, 3, 5), if_na = 10), 10)
 })
 
