e02f9949062134acb9f50ca61d9791bba0be5011
unknown <wuchouchouer@163.com>
2023-06-24 14:33:24 +0800

    add emoji to progress()

--- R/progress.R
+++ R/progress.R
@@ -11,9 +11,31 @@
 #'
 #' @export
 progress <- function(now, total) {
-  paste0(
-    "Learning Progress: ",
-    round(now / total * 100, 2),
-    "%."
+
+  emoji <- c(
+    "\U0001f600", # smile
+    "\U0001f973", # party face
+    "\U0001f638", # cat grin
+    "\U0001f308", # rainbow
+    "\U0001f947", # gold medal
+    "\U0001f389", # party popper
+    "\U0001f38a"  # confetti ball
   )
+
+  if (now >= total) {
+    paste0(
+      "Learning Progress: ",
+      round(now / total * 100, 2),
+      "%.",
+      sample(emoji, 1)
+    )
+  } else {
+    paste0(
+      "Learning Progress: ",
+      round(now / total * 100, 2),
+      "%."
+    )
+  }
 }
+
+
