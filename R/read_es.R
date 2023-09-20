242338e897579f3f0dfb69188fcf6889df48a66d
Shitao5 <wuchouchouer@163.com>
2023-09-20 15:48:18 +0800

    read_es_query

---
+++ R/read_es.R
@@ -0,0 +1,28 @@
+#' Read Data from Elasticsearch Query JSON
+#'
+#' This function is used to extract data from a JSON file containing the results of an Elasticsearch query. It converts the results into a tibble, then flattens the '_source' column and removes the '_index', '_id', and '_score' columns.
+#'
+#' @param json_file The path to the JSON file containing the Elasticsearch query results.
+#' @return Returns a tibble containing the data extracted from the query results.
+#'
+#' @examples
+#' \dontrun{
+#' # Read Elasticsearch query results from a JSON file
+#' query_result <- read_es_query("path/to/query_result.json")
+#'
+#' # View the first few rows of data
+#' head(query_result)
+#' }
+#'
+#' @import jsonlite
+#' @import dplyr
+#' @import tidyr
+#'
+#' @export
+read_es_query <- function(json_file) {
+  json = jsonlite::fromJSON(json_file)
+  json$hits$hits %>%
+    as_tibble() %>%
+    unnest_wider(`_source`) %>%
+    select(-c(`_index`, `_id`, `_score`))
+}
